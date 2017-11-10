3262d538fe05950c77662f6eca30859e85b3b61d
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-11-09 20:30:00 -0500

    Inital Setup of Lesson on Node and Mocha Testing.

---
+++ test/first_spec.js
@@ -0,0 +1,9 @@
+// Example Test that is hard coded to true.
+var assert = require('assert');
+describe('A Feature', function() {
+	describe('A scenario', function() {
+		it('does something expected', function() {
+			assert('true');
+		});
+	});
+});
