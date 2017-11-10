9246b5792ab7e9f2bf1f162386b604a3bf51e377
Nathan Stanford Sr <Nathan.Stanford@gmail.com>
2017-11-09 23:41:03 -0500

    Start building the real eacmple.

--- test/first_spec.js
+++
@@ -1,9 +0,0 @@
-// Example Test that is hard coded to true.
-var assert = require('assert')
-describe('A Feature', function () {
-  describe('A scenario', function () {
-    it('does something expected', function () {
-      assert('true')
-    })
-  })
-})
